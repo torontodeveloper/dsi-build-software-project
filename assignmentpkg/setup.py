41ce8a20072b02bbb82d1074ec548569c0537e22
Kakolla <kevin.kakolla@fujitsu.com>
2024-01-09 20:08:35 -0500

    missed file

--- assignmentpkg/setup.py
+++ assignmentpkg/setup.py
@@ -2,4 +2,5 @@ from setuptools import setup
 
 setup(name='assignment',
       version='2.0.0',author='Kevin',
-      packages=['assignmentpkg'])
+      packages=['assignmentpkg'],
+      install_requires=['requests'])
