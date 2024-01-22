4c968fe329f68c008592e358636b259c46ea78eb
Kakolla <kevin.kakolla@fujitsu.com>
2024-01-22 10:26:01 -0500

    setup file fix

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from setuptools import setup
 setup(name='assignment',
       version='2.0.0',author='Kevin,Ashish,Rahul,Sanjam',
       packages=['assignmentpkg'],
-      install_requires=['requests','pandas','yaml', 'matplotlib'])
+      install_requires=['requests','pandas','pyyaml', 'matplotlib'])
